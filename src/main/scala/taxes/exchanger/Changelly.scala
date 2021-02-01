b9ede26cd266df7e07b1622dc81176861991054b
cryptoTaxes <cryptoTaxes@protonmail.com>
2021-02-01 02:58:25 +0100

    Add links for Changelly descriptions.

--- src/main/scala/taxes/exchanger/Changelly.scala
+++ src/main/scala/taxes/exchanger/Changelly.scala
@@ -21,16 +21,28 @@ object Changelly extends Exchanger {
     return (amount, currency)
   }
 
+  private var txCounter = 0
+  case class Key(fromAmount: Double, toAmount: Double, txOrder: Int)
+  private val descriptions = collection.mutable.Map[Key, RichText]()
+
   override val sources = Seq(
-    new FilteredUserInputFolderSource[Operation]("changelly/2017", ".csv") {
-      def fileSource(fileName: String) = operationsReader2017(fileName)
-    },
     new FilteredUserInputFolderSource[Operation]("changelly/2017/depositsWithdrawals", ".txt") {
       def fileSource(fileName: String)= new FileSource[Operation](fileName) {
         override def read(): Seq[Operation] =
           readDepositsWithdrawals2017(fileName)
       }
-    }, /* Exchanges can be properly obtained from deposits/withdrawals text file
+    },
+    new FilteredUserInputFolderSource[Operation]("changelly/2017", ".csv") {
+      def fileSource(fileName: String) = operationsReader2017(fileName)
+    },
+    /*
+    new FilteredUserInputFolderSource[Operation]("changelly/2017/depositsWithdrawals", ".txt") {
+      def fileSource(fileName: String)= new FileSource[Operation](fileName) {
+        override def read(): Seq[Operation] =
+          readDepositsWithdrawals2017Full(fileName)
+      }
+    },*/
+    /* Exchanges can be properly obtained from deposits/withdrawals text file
     new UserInputYearFolderSource[Operation]("changelly/2020", ".csv") {
       def fileSource(fileName: String) = operationsReader2020(fileName)
     }, */
@@ -48,6 +60,7 @@ object Changelly extends Exchanger {
     override def lineScanner(line: String): Scanner =
       QuotedScanner(line, '\"', ',')
 
+    var lineNumber = 0
     override def readLine(line: String, scLn: Scanner): CSVReader.Result[Operation] = {
       val status = scLn.next("Status")
       if(status=="finished") {
@@ -70,7 +83,9 @@ object Changelly extends Exchanger {
         val realFee = fromAmount * exchangeRate - toAmount
         val feePercent = realFee * 100 / (fromAmount * exchangeRate)
 
-        val desc = RichText(s"Order: $receiverWallet")
+        val desc = descriptions(Key(fromAmount, toAmount, txCounter-lineNumber-1))
+                    //RichText(s"Order: $receiverWallet")
+        lineNumber += 1
 
         val exchange = Exchange(
             date = date
@@ -87,43 +102,6 @@ object Changelly extends Exchanger {
     }
   }
 
-  private def readDepositsWithdrawals2017(fileName: String): Seq[Operation] = {
-    def parseDate(str: String) =
-      LocalDateTime.parseAsMyZoneId(str, "dd MMM yyyy, HH:mm:ss")
-
-    FileSystem.withSource(fileName) { src =>
-      val lines = src.getLines().filterNot(taxes.util.parse.Parse.isComment)
-      val operations = ListBuffer[Operation]()
-      while(lines.hasNext) {
-        val Array(_,id,_,inHash,_,inAmountLine,_,inDate,_,feeLine,_,_,_,receiver,_,outHash,_,outAmountLine,_,outDate) = lines.take(20).toArray
-
-        val (inAmount, inCurrency) = split(inAmountLine)
-        val (feeAmount, feeCurrency) = split(feeLine)
-        val (outAmount, outCurrency) = split(outAmountLine)
-
-        val deposit = Deposit(
-          parseDate(inDate)
-          , id = id
-          , amount = inAmount
-          , currency = inCurrency
-          , exchanger = Changelly
-          , description = RichText(s"Deposit ${RichText.util.transaction(inCurrency, inHash)}")
-        )
-
-        val withdrawal = Withdrawal(
-          parseDate(outDate)
-          , id = id
-          , amount = outAmount
-          , currency = outCurrency
-          , exchanger = Changelly
-          , description = RichText(s"Withdrawal ${RichText.util.transaction(outCurrency, outHash, receiver)}")
-        )
-        operations += deposit
-        operations += withdrawal
-      }
-      return operations.toList
-    }
-  }
 
   private def operationsReader2020(fileName: String) = new CSVSortedOperationReader(fileName) {
     override val linesToSkip = 1
@@ -166,6 +144,21 @@ object Changelly extends Exchanger {
     }
   }
 
+  private def makeDescription(orderId: String, inCurrency: Currency, inHash: String, outCurrency: Currency, outHash: String): RichText = {
+    var desc = RichText(s"Order: $orderId")
+
+    BlockExplorer.transactionURL(inCurrency, inHash) match {
+      case None => ;
+      case Some(url) => desc = desc + " " + RichText.url("in ", url)
+    }
+
+    BlockExplorer.transactionURL(outCurrency, outHash) match {
+      case None => ;
+      case Some(url) => desc = desc + " " + RichText.url("out ", url)
+    }
+    return desc
+  }
+
   private def readDepositsWithdrawals2020(fileName: String): Seq[Operation] = {
     def parseDate(str: String) =
       LocalDateTime.parseAsMyZoneId(str, "dd MMM yyyy, HH:mm:ss")
@@ -174,7 +167,7 @@ object Changelly extends Exchanger {
       val lines = src.getLines().filterNot(taxes.util.parse.Parse.isComment)
       val operations = ListBuffer[Operation]()
       while(lines.hasNext) {
-        val array@Array(_,id,_,_,inAmountLine,_,outAmountLine,_,inDate,_,outDate,_,exchRateLine,_,receiver,_,inHash,_,outHash) = lines.take(19).toArray
+        val array@Array(_,orderId,_,_,inAmountLine,_,outAmountLine,_,inDate,_,outDate,_,exchRateLine,_,receiver,_,inHash,_,outHash) = lines.take(19).toArray
 
         val (inAmount, inCurrency) = split(inAmountLine)
         val (outAmount, outCurrency) = split(outAmountLine)
@@ -194,21 +187,138 @@ object Changelly extends Exchanger {
 
         val deposit = Deposit(
           date = depositDate
-          , id = id
+          , id = orderId
           , amount = inAmount
           , currency = inCurrency
           , exchanger = Changelly
           , description = RichText(s"Deposit ${RichText.util.transaction(inCurrency, inHash)}")
         )
 
+        val desc = makeDescription(orderId, inCurrency, inHash, outCurrency, outHash)
+
         val exchange = Exchange(
             date = depositDate.plusNanos(1) // to keep proper order for deposit/exchange/withdrawal
-            , id = id
+            , id = orderId
             , fromAmount = fromAmount, fromCurrency = inCurrency
             , toAmount = toAmount, toCurrency = outCurrency
             , fees = List(FeePair(fee, inCurrency))
             , exchanger = Changelly
-            , description = RichText(s"Order: $id")
+            , description = desc
+        )
+
+        val withdrawal = Withdrawal(
+          parseDate(outDate)
+          , id = orderId
+          , amount = outAmount
+          , currency = outCurrency
+          , exchanger = Changelly
+          , description = RichText(s"Withdrawal ${RichText.util.transaction(outCurrency, outHash, receiver)}")
+        )
+        operations += deposit
+        operations += exchange
+        operations += withdrawal
+      }
+      return operations.toList
+    }
+  }
+
+  private def round(value: Double, places: Int): Double = {
+    if (places < 0) throw new IllegalArgumentException
+    var bd = BigDecimal.valueOf(value)
+    bd = bd.setScale(places, scala.math.BigDecimal.RoundingMode.DOWN)
+    bd.doubleValue
+  }
+
+  private def readDepositsWithdrawals2017(fileName: String): Seq[Operation] = {
+    def parseDate(str: String) =
+      LocalDateTime.parseAsMyZoneId(str, "dd MMM yyyy, HH:mm:ss")
+
+    FileSystem.withSource(fileName) { src =>
+      val lines = src.getLines().filterNot(taxes.util.parse.Parse.isComment)
+      val operations = ListBuffer[Operation]()
+      while(lines.hasNext) {
+        val array@Array(_,orderId,_,inHash,_,inAmountLine,_,inDate,_,feeLine,_,exchRateLine,_,receiver,_,outHash,_,outAmountLine,_,outDate) = lines.take(20).toArray
+
+        val (inAmount, inCurrency) = split(inAmountLine)
+        val (outAmount, outCurrency) = split(outAmountLine)
+        val (feeAmount, feeCurrency) = split(feeLine)
+
+        val (token1, token2) = Parse.split(exchRateLine, " = ")
+        val (rateSold, rateSoldCurrency) = split(token1)
+        val (rateReceived, rateReceivedCurrency) = split(token2)
+
+        if(inCurrency != rateSoldCurrency || outCurrency != rateReceivedCurrency)
+          Logger.fatal(s"${Changelly.id}. Read file ${FileSystem.pathFromData(fileName)}: cannot parse these lines: ${array.mkString("\n")} as rate is not expressed as $outCurrency/$inCurrency.")
+
+        val fromAmount = round(inAmount, 8)
+        var toAmount = round(outAmount, 8)
+        val exchangeRate = round((toAmount+feeAmount)/fromAmount, 8)
+        if(outCurrency == Currency.ripple || outCurrency == Currency.normalize("STRAT"))
+          toAmount = round(outAmount, 2)
+
+        val realFee = fromAmount * exchangeRate - toAmount
+
+        val depositDate = parseDate(inDate)
+
+        val desc = makeDescription(orderId, inCurrency, inHash, outCurrency, outHash)
+        descriptions += (Key(fromAmount, toAmount, txCounter) -> desc)
+        txCounter += 1
+
+        val deposit = Deposit(
+          date = depositDate
+          , id = orderId
+          , amount = inAmount
+          , currency = inCurrency
+          , exchanger = Changelly
+          , description = RichText(s"Deposit ${RichText.util.transaction(inCurrency, inHash)}")
+        )
+        /*
+        val exchange = Exchange(
+          date = depositDate.plusNanos(1) // to keep proper order for deposit/exchange/withdrawal
+          , id = orderId
+          , fromAmount = fromAmount, fromCurrency = inCurrency
+          , toAmount = toAmount, toCurrency = outCurrency
+          , fees = List(FeePair(realFee, feeCurrency))
+          , exchanger = Changelly
+          , description = desc
+        )*/
+
+        val withdrawal = Withdrawal(
+          parseDate(outDate)
+          , id = orderId
+          , amount = outAmount
+          , currency = outCurrency
+          , exchanger = Changelly
+          , description = RichText(s"Withdrawal ${RichText.util.transaction(outCurrency, outHash, receiver)}")
+        )
+        operations += deposit
+        operations += withdrawal
+      }
+      return operations.toList
+    }
+  }
+
+  private def readDepositsWithdrawals2017OLD(fileName: String): Seq[Operation] = {
+    def parseDate(str: String) =
+      LocalDateTime.parseAsMyZoneId(str, "dd MMM yyyy, HH:mm:ss")
+
+    FileSystem.withSource(fileName) { src =>
+      val lines = src.getLines().filterNot(taxes.util.parse.Parse.isComment)
+      val operations = ListBuffer[Operation]()
+      while(lines.hasNext) {
+        val Array(_,id,_,inHash,_,inAmountLine,_,inDate,_,feeLine,_,_,_,receiver,_,outHash,_,outAmountLine,_,outDate) = lines.take(20).toArray
+
+        val (inAmount, inCurrency) = split(inAmountLine)
+        val (feeAmount, feeCurrency) = split(feeLine)
+        val (outAmount, outCurrency) = split(outAmountLine)
+
+        val deposit = Deposit(
+          parseDate(inDate)
+          , id = id
+          , amount = inAmount
+          , currency = inCurrency
+          , exchanger = Changelly
+          , description = RichText(s"Deposit ${RichText.util.transaction(inCurrency, inHash)}")
         )
 
         val withdrawal = Withdrawal(
@@ -220,7 +330,6 @@ object Changelly extends Exchanger {
           , description = RichText(s"Withdrawal ${RichText.util.transaction(outCurrency, outHash, receiver)}")
         )
         operations += deposit
-        operations += exchange
         operations += withdrawal
       }
       return operations.toList
